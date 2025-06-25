94f723259b09a5bf0eb15314698397c744a2a8eb
hj522 <11_22nd@naver.com>
2025-06-25 22:15:05 +0900

    Add Login, Sign up, Profile components

--- frontend/src/layout/AppLayout.tsx
+++ frontend/src/layout/AppLayout.tsx
@@ -4,33 +4,34 @@ import Loading from '../components/Loading';
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-
 const Home = React.lazy(() => import('../pages/Home'));
 const Write = React.lazy(() => import('../pages/Write'));
 const MyPosts = React.lazy(() => import('../pages/MyPosts'));
 const AllPosts = React.lazy(() => import('../pages/AllPosts'));
-const Login = React.lazy(() =>  import('../pages/Login'));
+const Login = React.lazy(() => import('../pages/Login'));
+const SignUp = React.lazy(() => import('../pages/SignUp'));
 const Profile = React.lazy(() => import('../pages/Profile'));
 
 const AppLayout: React.FC = () => {
-  return (
-     <div>
-      <NavBar />
-      <main style={{ padding: '20px' }}>
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/write" element={<Write />} />
-            <Route path="/my-posts" element={<MyPosts />} />
-            <Route path="/all-posts" element={<AllPosts />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Suspense>
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <main style={{ padding: '20px' }}>
+                <Suspense fallback={<Loading />}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/write" element={<Write />} />
+                        <Route path="/my-posts" element={<MyPosts />} />
+                        <Route path="/all-posts" element={<AllPosts />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/sign-up" element={<SignUp />} />
+                    </Routes>
+                </Suspense>
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default AppLayout;
